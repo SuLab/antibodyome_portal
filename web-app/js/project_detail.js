3d99e15b88b91abd97c43813a5ae41fa63f87eb9
jin xuefeng <172174484@qq.com>
2014-05-28 20:14:51 +0800

    bug in page

--- web-app/js/project_detail.js
+++ web-app/js/project_detail.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
                         $('#notification-modal').modal('hide');
                     });
                 }
-            }
+            });
         });
 });
 
