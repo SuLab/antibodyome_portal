59b615f8caddf0fff341acc4def61f49673c9dc4
Chunlei Wu <newgene@users.noreply.github.com>
2014-06-09 07:26:16 +0000

    proces analyze result as dictionary

--- src/upload/management/commands/start_analyze.py
+++ src/upload/management/commands/start_analyze.py
@@ -52,6 +52,7 @@ class Analysis(threading.Thread):
 
     def do_analysis(self, p):
         # import pdb;pdb.set_trace()
+        print p
         samples = p.sample_set.filter(status__in=[Project.STATUS_READY, Project.STATUS_ANALYZING])
         if samples is not None:
             # project in analyzing status
@@ -61,12 +62,15 @@ class Analysis(threading.Thread):
             ab = self.conn.root
             for s in samples:
                 # update local status
+                self.status = [p.id, s.id, 0]
                 if s.status == Project.STATUS_READY:
-                    self.status = [p.id, s.id, 0]
+                    print 'sample status  is ready'
                     try:
                         key = str("s3:"+s.uuid)
+                        print 'run igblast, key:%s'%key
                         # Start analysis sample's3:raw_data/2064_g1.fasta.gz'
                         job_id = ab.run_igblast(infile=key)
+                        print 'igblast ok'
                         s.job_id = job_id
                         s.status = Project.STATUS_ANALYZING
                         s.save()
@@ -76,19 +80,26 @@ class Analysis(threading.Thread):
                         s.save()
                         continue
                 else:
+                    print 'sample is analyzing'
                     job_id = s.job_id
 
                 while True:
+                    print 'job_id: %s' %job_id
                     pro = ab.check_progress(job_id)
-                    if len(pro) == 1 and pro[0][0] == u'SUCCESS':
+                    print pro
+                    pro = dict(pro)
+                    total = 0
+                    for k in pro:
+                         total = total + pro[k]
+                    if total == pro['SUCCESS']:
                         print "progress 100%"
                         s.status = Project.STATUS_ANALYZED
                         self.status[2] = 100
                         s.save()
                         break
                     else:
-                        print "progress  " + str(int(float(pro[1][1])/float(pro[0][1]+pro[1][1])*100)) + "%"
-                        self.status[2] = int(float(pro[1][1])/float(pro[0][1]+pro[1][1])*100)
+                        print "progress  " + str(100*pro['SUCCESS']/total) + "%"
+                        self.status[2] = 100*pro['SUCCESS']/total
                     # sleep a while before next check
                     time.sleep(self.SLEEP_TIME)
 
@@ -100,17 +111,21 @@ class Analysis(threading.Thread):
             p.save()
             if p.status == Project.STATUS_ANALYZED:
                 '''python -m smtpd -n -c DebuggingServer localhost:1025'''
-                send_mail('hello', 'welcome to register.', 'localhost@example.com', [p.owner.email], fail_silently=False)
+                #send_mail('hello', 'welcome to register.', 'localhost@example.com', [p.owner.email], fail_silently=False)
 
     def get_status(self):
         return self.status
 
     def run(self):
+        print 'setup remote host'
         self.setup_remote_host()
+        print 'host ready'
         while not self.stop:
             target = self.get_analysis_target()
             if target is not None:
+                print 'acquire workers'
                 self.prepare_workers()
+                print  'analyze project: '+str(target)
                 self.do_analysis(target)
             else:
                 self.finish_workers()
