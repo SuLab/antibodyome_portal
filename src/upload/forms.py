1677207bca93efa0ac0d323badda5cc899ae7459
jin xuefeng <172174484@qq.com>
2014-08-18 16:58:21 +0800

    remove useless files and separate s3 method to new file

--- src/upload/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from upload.models import Project
-
-
-class ProjectForm(ModelForm):
-    class Meta:
-        model = Project
-        fields = ('title', 'permission', 'organism', 'summary')
