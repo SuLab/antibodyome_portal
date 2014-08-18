97b1946fd4bbac87617c1e9fc185d87c5f9b5cbf
jin xuefeng <172174484@qq.com>
2014-08-18 16:44:56 +0800

    refine code

--- src/upload/urls.py
+++ src/upload/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 import views
+import s3_views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -16,21 +17,26 @@ urlpatterns = patterns(
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^sign-policy', views.handle_s3_POST, name="sign-policy-form-POST"),
+    url(r'^sign-policy', s3_views.handle_s3_POST, \
+        name="sign-policy-form-POST"),
+    url(r'^delete-file/(?P<key>.+)$', s3_views.delete_s3_file,\
+         name="delete-s3-file"),
     url(r'^create-project/', views.create_project, name="create-project"),
-    #url(r'^create-project-success/', views.create_project_success, name="create-project-success"),
-    url(r'^update-project/(?P<abp_id>.+)', views.update_project, name="update_project"),
-    url(r'^delete-sample/(?P<id>\d+)', views.delete_sample, name="delete-sample"),
-    url(r'^project/(?P<pk>\d+)/$', views.ProjectDetail.as_view(), name="project-detail"),
+    url(r'^update-project/(?P<abp_id>.+)', views.update_project, \
+        name="update_project"),
+    url(r'^delete-sample/(?P<id>\d+)', views.delete_sample, \
+        name="delete-sample"),
+    url(r'^project/(?P<pk>\d+)/$', views.ProjectDetail.as_view(), \
+        name="project-detail"),
     url(r'^project/(?P<abp_id>.+)/$', views.ABProjectDetail),
     url(r'^project-list/$', views.ProjectList.as_view(), name="project-list"),
-    url(r'^project-analysis/(?P<abp_id>.+)/$', views.submit_analyze, name="project-submit-analyze"),
+    url(r'^project-analysis/(?P<abp_id>.+)/$', views.submit_analyze, \
+        name="project-submit-analyze"),
     url(r'^project-analysis/(?P<slug>.+)/$', views.submit_analyze),
-    url(r'^delete-file/(?P<key>.+)$', views.delete_s3_file, name="delete-s3-file"),
     url(r'^sample-ab/(?P<abs_id>.+)/$', views.sample_ab, name="sample-ab"),
     url(r'^random-ab/(?P<abs_id>.+)/$', views.random_ab, name="random-ab"),
-    url(r'^list-ab/(?P<abs_id>.+)/$', views.list_ab, name="list-ab"),
-    url(r'^count-ab/(?P<abs_id>.+)/$', views.count_ab, name="count-ab"),
+    url(r'^ab-list/(?P<abs_id>.+)/$', views.list_ab, name="list-ab"),
+    url(r'^ab-count/(?P<abs_id>.+)/$', views.count_ab, name="count-ab"),
     url(r'^ab-detail/$', views.ab_detail, name="ab-detail"),
     url(r'^convert-svg/$', views.convert_svg, name="convert-svg"),
     url(r'^file-down/$', views.file_download, name="file-download"),
